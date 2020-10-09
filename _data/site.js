faa2c7b87df08ea7ba07613040ca0059d0468579
afenker1985 <aaronfenker@gmail.com>
2020-10-08 19:58:32 -0500

    Update site.js

--- _data/site.js
+++ _data/site.js
@@ -1,6 +1,6 @@
 module.exports = () => ({
-    title: "my website",
-    author: "Your Name",
+    title: "The BBQ Rev",
+    author: "Aaron Fenker",
     description: "My personal website",
     // If an env variable (local dev) has been set, use it, otherwise default (prod)
     url:  process.env.SITE_URL ? process.env.SITE_URL : "https://thebbqrev.com"
