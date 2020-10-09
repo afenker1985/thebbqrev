7909908ef45fd1ac635ada1e9e0dfce65554a64b
afenker1985 <aaronfenker@gmail.com>
2020-10-09 11:20:11 -0500

    Update site.js

--- _data/site.js
+++ _data/site.js
@@ -1,7 +1,7 @@
 module.exports = () => ({
     title: "The BBQ Rev",
     author: "Aaron Fenker",
-    description: "My personal website",
+    description: "",
     // If an env variable (local dev) has been set, use it, otherwise default (prod)
     url:  process.env.SITE_URL ? process.env.SITE_URL : "https://thebbqrev.com"
 });
