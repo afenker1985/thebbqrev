8f7bd7168b6d8f29fa2a8578fc5f0ee2678ca4ae
afenker1985 <aaronfenker@gmail.com>
2020-10-13 08:07:52 -0500

    Update site.js

--- _data/site.js
+++ _data/site.js
@@ -4,4 +4,5 @@ module.exports = () => ({
   description: "",
   // If an env variable (local dev) has been set, use it, otherwise default (prod)
   url: process.env.SITE_URL ? process.env.SITE_URL : "https://thebbqrev.com",
+  time: getFullYear(),
 });
